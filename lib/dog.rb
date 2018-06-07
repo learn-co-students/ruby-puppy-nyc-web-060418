126d3527cbf6ecd8ab11014035b6f7896dc34504
Ms-Alex <ms.hernandeza1@gmail.com>
2018-06-07 09:12:25 -0400

    Finished, will push

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Dog
+
+  attr_accessor :name
+  @@all = []
+
+def initialize(name)
+  @name = name
+  @@all << self
+
+end
+
+def self.clear_all
+  @@all.clear
+end
+
+def self.all
+  @@all.each do |name|
+    puts name.name
+  end
+end
+
+end
+
+
+lala = Dog.new("Lala")
+snoopy = Dog.new("Snoopy")
+tiger = Dog.new("Tiger")
