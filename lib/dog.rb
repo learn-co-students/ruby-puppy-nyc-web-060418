17c1b1a9db32e30aec1a7803529bf64aca524565
Changmin Lim <chang.lim92@gmail.com>
2018-06-07 20:43:27 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  @@all = [] 
+  attr_accessor :name 
+  def initialize(name)
+    @@all << self 
+    @name = name 
+  end 
+  
+  def self.clear_all
+    @@all = []
+  end 
+  
+  def self.all 
+    @@all.each do |dog|
+      puts dog.name
+    end 
+  end 
+end 
