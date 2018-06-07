711292f2dbdbab1469149b5e19a59e6a5191b408
jarretbryan <jarret.bryan@gmail.com>
2018-06-06 22:38:50 -0400

    defined Dog class

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name=name
+    @@all<<self
+  end
+
+  def name
+    @name
+  end
+
+  def self.all
+    @@all
+    @@all.each do |instance|
+      puts instance.name
+    end
+  end
+
+  def self.clear_all
+      @@all.clear
+  end
+
+end
